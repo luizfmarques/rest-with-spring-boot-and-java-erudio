cae0b47ce2c3d25b3c9565d556a9b6341938e6bb
luizfmarques <luiz_icb@hotmail.com>
2023-10-04 20:25:58 -0300

    First steps in java with springboot 3

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/lfm/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.lfm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
